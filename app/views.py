d6d6807dbefc784898ff91616449c78e55ada04f
sagibu <sagibuch93@gmail.com>
2017-07-09 00:15:39 +0300

    Added monitor to API

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import request
+import json
+from FrequentWordCalculator import FrequentWordCalculator
+
+frequent_words = FrequentWordCalculator('settings/settings.json')
 
 @app.route('/')
 @app.route('/index')
 def index():
+    print("ABC")
     return "Hellao, World!"
+
+@app.route('/monitor/<topic>', methods=['POST'])
+def monitor(topic):
+    frequent_words.monitor_topic(topic, 1800.0)
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
