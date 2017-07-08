997393cbbe220f12d5eb559f414db04427ee43d8
sagibu <sagibuch93@gmail.com>
2017-07-08 23:26:55 +0300

    Start adding flask web server

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+#!flask/bin/python
 from FrequentWordCalculator import FrequentWordCalculator
 import time
+from app import app
 
 
 def main():
-    frequent_words = FrequentWordCalculator('settings.json')
+    # frequent_words = FrequentWordCalculator('settings/settings.json')
     # frequent_words.monitor_topic("dota", 1800.0)
     # frequent_words.monitor_topic("NBA", 1800.0)
     # frequent_words.monitor_topic("hearthstone", 1800.0)
-    print(frequent_words.most_frequent_words("dota"))
+    #
+    # while True:
+    #     print(frequent_words.most_frequent_words("dota"))
+    #     print(frequent_words.most_frequent_words("NBA"))
+    #     print(frequent_words.most_frequent_words("hearthstone"))
+    #     time.sleep(100)
 
-    while True:
-        time.sleep(100)
+    app.run(debug=True)
 
 if __name__ == "__main__":
     main()
